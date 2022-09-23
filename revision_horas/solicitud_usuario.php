9259e4fd32e22003c06e703901727075e80e548e
ErikGmz <erikgomez198755@outlook.com>
2022-09-23 14:28:54 -0500

    Implementación de selección automática para el ID del colaborador.

--- revision_horas/solicitud_usuario.php
+++ revision_horas/solicitud_usuario.php
@@ -118,11 +118,23 @@
         </div>
 
         <!--Scripts de la página-->
-        <script type="text/javascript">
-            document.getElementById("fecha-final").value = new Date().toISOString().substring(0, 10);
-        </script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+        <script src="../js/enfocar_recuadro.js"> </script>
+        <script type="text/javascript">
+            document.getElementById("fecha-final").value = new Date().toISOString().substring(0, 10);
+
+            // Seleccionar automáticamente el recuadro
+            // del identificador del colaborador al
+            // escoger el tipo de revisión.
+            document.getElementById("resumidas").addEventListener("click", () => {
+                enfocarRecuadro("ID-colaborador");
+            });
+
+            document.getElementById("desglosadas").addEventListener("click", () => {
+                enfocarRecuadro("ID-colaborador");
+            });
+        </script>
     </body>
 </html>
